5445fd9053192f4604f9d9f5c3945ce249318401
kadle <kadle@email.com>
2024-09-26 12:38:10 +0530

    Added attachments functionality

--- gmail.py
+++ gmail.py
@@ -1,10 +1,14 @@
 import os
+import base64
+import mimetypes
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from email.mime.text import MIMEText
-import base64
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
@@ -28,11 +32,34 @@ def get_gmail_service():
 
     return build('gmail', 'v1', credentials=creds)
 
-def create_message(sender, to, subject, message_text):
-    message = MIMEText(message_text)
+def create_message(sender, to, subject, message_text, file_paths=None):
+    if file_paths:
+        message = MIMEMultipart()
+        message.attach(MIMEText(message_text))
+    else:
+        message = MIMEText(message_text)
+
     message['to'] = to
     message['from'] = sender
     message['subject'] = subject
+
+    if file_paths:
+        for file_path in file_paths:
+            content_type, encoding = mimetypes.guess_type(file_path)
+
+            if content_type is None or encoding is not None:
+                content_type = 'application/octet-stream'
+            main_type, sub_type = content_type.split('/', 1)
+
+            with open(file_path, 'rb') as file:
+                attachment = MIMEBase(main_type, sub_type)
+                attachment.set_payload(file.read())
+
+            encoders.encode_base64(attachment)
+            file_name = os.path.basename(file_path)
+            attachment.add_header('Content-Disposition', 'attachment', filename=file_name)
+            message.attach(attachment)
+
     raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode('utf-8')
     return {'raw': raw_message}
 
@@ -48,10 +75,11 @@ def send_message(service, user_id, message):
 def main():
     service = get_gmail_service()
     sender = "your_email@gmail.com"
-    to = "sender@example.com"
+    to = "recipient@example.com"
     subject = "Test Email from Gmail API"
     message_text = "This is a test email sent using the Gmail API."
-    
+    # Add your file paths here if you want to add attachments
+    ##file_paths = ["/path/to/attachment1.pdf", "/path/to/attachment2.jpg"]
     message = create_message(sender, to, subject, message_text)
     send_message(service, "me", message)
 
